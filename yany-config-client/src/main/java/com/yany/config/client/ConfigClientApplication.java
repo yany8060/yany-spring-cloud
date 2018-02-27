4602baca0081f7b10d762df5522e7660f6242938
yany8060 <806055720@qq.com>
2018-02-27 19:58:37 +0800

    spring cloud config client

--- yany-config-client/src/main/java/com/yany/config/client/ConfigClientApplication.java
+++ yany-config-client/src/main/java/com/yany/config/client/ConfigClientApplication.java
@@ -1,7 +1,15 @@
 package com.yany.config.client;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by yanyong on 2018/2/27.
  */
+@SpringBootApplication
 public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class);
+    }
 }
--- yany-config-client/src/main/java/com/yany/config/client/controller/ConfigClientController.java
+++ yany-config-client/src/main/java/com/yany/config/client/controller/ConfigClientController.java
@@ -1,7 +1,21 @@
 package com.yany.config.client.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by yanyong on 2018/2/27.
  */
+@RestController
 public class ConfigClientController {
+
+    @Value("${yany.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String configTest(){
+        return hello;
+    }
+
 }
