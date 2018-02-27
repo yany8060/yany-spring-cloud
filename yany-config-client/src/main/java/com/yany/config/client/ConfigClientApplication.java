4b9906717f12559cae9093ea1122c55d6d757367
yany8060 <806055720@qq.com>
2018-02-27 19:52:14 +0800

    spring cloud config client

---
+++ yany-config-client/src/main/java/com/yany/config/client/ConfigClientApplication.java
@@ -0,0 +1,7 @@
+package com.yany.config.client;
+
+/**
+ * Created by yanyong on 2018/2/27.
+ */
+public class ConfigClientApplication {
+}
---
+++ yany-config-client/src/main/java/com/yany/config/client/controller/ConfigClientController.java
@@ -0,0 +1,7 @@
+package com.yany.config.client.controller;
+
+/**
+ * Created by yanyong on 2018/2/27.
+ */
+public class ConfigClientController {
+}
