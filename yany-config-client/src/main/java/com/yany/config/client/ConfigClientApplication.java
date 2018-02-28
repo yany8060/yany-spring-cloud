5250c5156a6908366d1d0c4680930f672e41cebd
yany8060 <806055720@qq.com>
2018-02-28 15:09:26 +0800

    config 结合eureka实现高可用

--- yany-config-client/src/main/java/com/yany/config/client/ConfigClientApplication.java
+++ yany-config-client/src/main/java/com/yany/config/client/ConfigClientApplication.java
@@ -2,11 +2,13 @@ package com.yany.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by yanyong on 2018/2/27.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
--- yany-config-server/src/main/java/com/yany/config/server/ConfigServerApplication.java
+++ yany-config-server/src/main/java/com/yany/config/server/ConfigServerApplication.java
@@ -3,12 +3,14 @@ package com.yany.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by yanyong on 2018/2/27.
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
